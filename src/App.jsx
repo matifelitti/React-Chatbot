5884bcee65ff7ee998a24e4e8947066e5adb3c43
matifelitti <matiasfelitti@gmail.com>
2025-01-03 23:15:15 -0300

    installing tailwind, container div and chatbot title div

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-    </>
-  )
+    <div className="flex justify-center items-center shadow-lg">
+      <div className="w-96 text-center text-2xl text-indigo-50 font-medium bg-indigo-800/75">
+        Chatbot
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;

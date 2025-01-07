db4b08d4ea53dfe32697ce94a630381e7121c1e6
matifelitti <matiasfelitti@gmail.com>
2025-01-07 20:59:41 -0300

    chatbot title, form and button styles changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,27 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="flex justify-center items-center shadow-lg">
-      <div className="w-96 text-center text-2xl text-indigo-50 font-medium bg-indigo-800/75">
+    <div className="m-auto mt-8 w-80 h-96 col-auto justify-end shadow-lg border-solid border-2 border-teal-600/75">
+      <div className=" p-1 text-center text-2xl text-teal-50 font-medium bg-teal-600/75">
         Chatbot
       </div>
+      <div className="h-60">
+        <div className="mt-8 mb-4 ml-12 pt-2 pl-2 w-60 bg-teal-100/75 rounded">
+          Hello! How can I help you?
+        </div>
+      </div>
+
+      <form action="" className="flex items-center">
+        <input
+          type="text"
+          placeholder="message"
+          className="my-1 mb-2 ml-5 pt-2 pl-2 w-56 rounded border-2 border-[#0d9488] focus:outline-none"
+          required
+        />
+        <button className="my-1 mb-2 ml-3 px-2 w-12 h-9 text-white bg-teal-600/75 rounded">
+          ^
+        </button>
+      </form>
     </div>
   );
 }
